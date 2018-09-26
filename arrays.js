f0bc8dbd3d0b4a5073ddcda62ab1ed306d127f2d
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-26 19:34:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,5 @@ var destructivelyRemoveElementFromEndOfArray = function(array){
 };
 
 var removeElementFromEndOfArray = function(array){
-  array.slice(array.length -1);
+  return array.slice(array.length -1);
 };
