8f0a028aa3ca84b3a02659e9bc6bb1a317364ce5
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-26 19:01:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,15 +4,18 @@ var addElementToBeginningOfArray = function(array, element) {
   return [element,...array];
 };
 
-var addElementToEndOfArray = function (array, element) {
-  return[...array,element];
-};
-
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
   return array.unshift(element);
 };
 
 //destructivelyAddElementToBeginningOfArray(chocolateBars, "musk");
+
+var addElementToEndOfArray = function (array, element) {
+  return[...array,element];
+};
+
+
+
 
 var destructivelyAddElementToEndOfArray = function(array, element){
   return array.push(element);
